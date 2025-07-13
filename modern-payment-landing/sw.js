568802e4fb7287a9ab7ff0cee86cd1ed374b3477
Sm0k367 <139561920+Sm0k367@users.noreply.github.com>
2025-07-13 19:10:26 -0400

    Update sw.js

--- modern-payment-landing/sw.js
+++ modern-payment-landing/sw.js
@@ -1,1 +1,8 @@
+// Service Worker for PWA
+self.addEventListener('install', e => {
+  console.log('Service Worker: Installed');
+});
+self.addEventListener('fetch', e => {
+  // offline strategies...
+});
 
